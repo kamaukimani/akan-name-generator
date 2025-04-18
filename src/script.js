e0c8ebdf62aa36603a791383e26f2acd9d5f26d8
kamaukimani <paulkimys285@gmail.com>
2025-04-18 16:49:17 +0300

    add id to result div in index.html

--- src/script.js
+++ src/script.js
@@ -2,5 +2,9 @@ let form = document.getElementById("fm");
 form.addEventListener("submit",function(event) {
     event.preventDefault()
 })
+const result = document.getElementById("output")
 const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+const bday = document.getElementById("birthdate")
+bday = birthdateInput.value;
+
