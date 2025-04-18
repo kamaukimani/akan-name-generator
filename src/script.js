471f68acf36d5aecd03cd6af79672238d200aa52
kamaukimani <paulkimys285@gmail.com>
2025-04-18 16:41:37 +0300

    add akan female names in script.js

--- src/script.js
+++ src/script.js
@@ -3,3 +3,4 @@ form.addEventListener("submit",function(event) {
     event.preventDefault()
 })
 const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
