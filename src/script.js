35ffcb924f6f469d732c13bd551342033aa00a46
kamaukimani <paulkimys285@gmail.com>
2025-04-18 18:05:52 +0300

    modify script.js

--- src/script.js
+++ src/script.js
@@ -1,27 +1,33 @@
 let form = document.getElementById("fm");
+
 form.addEventListener("submit",function(event) {
-    event.preventDefault()
-})
-const result = document.getElementById("output")
+    event.preventDefault();
+
+const result = document.getElementById("output");
 const genderInput = document.querySelector('input[name="choose_gender"]:checked');
 const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-const bday = document.getElementById("birthdate")
-bday = birthdateInput.value;
-if (!bday) {
+const bday = document.getElementById("birthdate");
+const bdayValue = bday.value;
+
+if (!bdayValue) {
     result.innerText = "Enter a valid birthdate";
     return;
 }
-const date = new Date(bday);
+
+const date = new Date(bdayValue);
 if (isNaN(date.getTime()) || date > new Date()) {
-    result,innerText = "Enter a valid past date";
+    result.innerText = "Enter a valid past date";
     return;
 }
+
 if (!genderInput) {
     result.innerText = "Select gender";
     return;
 }
+
 const dayIndex = date.getDay();
 const genderValue = genderInput.value;
 const akanName = genderValue === "male" ? maleNames[dayIndex] : femaleNames[dayIndex];
-result.innerText = "Akan Name is: ${akanName}";
+result.innerText = `Akan Name is: ${akanName}`;
+});
