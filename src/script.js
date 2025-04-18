fc48e26c04b166627f6b83d34c19ef88dbedd688
kamaukimani <paulkimys285@gmail.com>
2025-04-18 17:11:50 +0300

    check validity of date

--- src/script.js
+++ src/script.js
@@ -7,7 +7,16 @@ const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 const bday = document.getElementById("birthdate")
 bday = birthdateInput.value;
-if (bday) {
-    result.innerText = "Enter a valid birthdate"
+if (!bday) {
+    result.innerText = "Enter a valid birthdate";
+    return;
+}
+const date = new Date(bday);
+if (isNaN(date.getTime()) || date > new Date()) {
+    result,innerText = "Enter a valid past date";
+    return;
+}
+if (!genderInput) {
+    result.innerText = "Select gender";
     return;
 }
