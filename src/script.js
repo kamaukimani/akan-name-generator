98b877e1f1b51d71778dc239544071b0aaae1094
kamaukimani <paulkimys285@gmail.com>
2025-04-18 16:38:46 +0300

    add male akan names in index.js

--- src/script.js
+++ src/script.js
@@ -2,3 +2,4 @@ let form = document.getElementById("fm");
 form.addEventListener("submit",function(event) {
     event.preventDefault()
 })
+const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
